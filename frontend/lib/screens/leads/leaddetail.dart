06c8287b55fd23219792936afcf7b63f8f5758f4
austin wambugu <mutonyiaustin@gmail.com>
2024-01-29 22:30:04 +0300

    edit lead

---
+++ frontend/lib/screens/leads/leaddetail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Leaddetail extends StatelessWidget {
+  const Leaddetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
--- frontend/lib/screens/leads/leadpage.dart
+++ frontend/lib/screens/leads/leadpage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
+import 'leaddetail.dart';
+
 class Leadpage extends StatelessWidget {
   const Leadpage({super.key});
 
@@ -12,19 +14,43 @@ class Leadpage extends StatelessWidget {
       ),
       body: Stack(
         children: [
-          Leadslist(
-            height: 98.h,
-            width: 98.w,
+          Positioned(
+            top: 8.2.h,
+            width: 99.w,
+            height: 90.h,
+            child: Leadslist(
+              height: 98.h,
+              width: 98.w,
+            ),
           ),
           Positioned(
-              bottom: 0.0001,
-              height: 4.h,
-              child: MaterialButton(
-                  height: 5.h,
-                  minWidth: 100.w,
-                  color: const Color.fromARGB(255, 17, 140, 107),
-                  child: const Text("add lead"),
-                  onPressed: () {}))
+            bottom: 0.0001,
+            height: 4.h,
+            child: MaterialButton(
+                height: 5.h,
+                minWidth: 100.w,
+                color: const Color.fromARGB(255, 17, 140, 107),
+                child: const Text("add lead"),
+                onPressed: () {}),
+          ),
+          Positioned(
+            top: 0.0001,
+            height: 3.5.h,
+            child: Container(
+              color: Colors.amber,
+              height: 3.5.h,
+              width: 100.w,
+            ),
+          ),
+          Positioned(
+            top: 3.6.h,
+            height: 3.5.h,
+            child: Container(
+              color: const Color.fromARGB(255, 101, 86, 39),
+              height: 3.5.h,
+              width: 100.w,
+            ),
+          ),
         ],
       ),
     );
@@ -43,16 +69,26 @@ class Leadslist extends StatelessWidget {
         child: ListView.builder(
           itemCount: 10,
           itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("name"),
-                  Text("072323233"),
-                  Text("status"),
-                  Text("assignedto"),
-                ],
+            return InkWell(
+              onTap: () {
+                Navigator.push<void>(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => const Leaddetail(),
+                  ),
+                );
+              },
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("name"),
+                    Text("072323233"),
+                    Text("status"),
+                    Text("assignedto"),
+                  ],
+                ),
               ),
             );
           },
