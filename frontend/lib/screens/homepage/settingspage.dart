1683c6c142519a0abd842bd47e793fb4e8483ef7
austin wambugu <mutonyiaustin@gmail.com>
2024-02-02 23:27:13 +0300

    ui

---
+++ frontend/lib/screens/homepage/settingspage.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class Settingspage extends StatelessWidget {
+  const Settingspage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 10.w,
+              child: Icon(
+                Icons.face,
+                size: 31.sp,
+              ),
+            ),
+            Card(
+              child: SizedBox(
+                height: 11.h,
+                width: 70.w,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 8.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        "mutonyiaustin@gmail.com",
+                        style: TextStyle(fontSize: 18.sp),
+                      ),
+                      Text(
+                        "role: Admin ",
+                        style: TextStyle(fontSize: 18.sp),
+                      ),
+                      Text(
+                        "permissions: ",
+                        style: TextStyle(fontSize: 18.sp),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const Divider(),
+            Settitem(
+              title: "theme",
+              icon: Icons.sunny,
+              ontap: () {
+                print("theme");
+              },
+            ),
+            const Divider(),
+            Settitem(
+              title: "logout",
+              icon: Icons.logout,
+              ontap: () {
+                print("logout");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Settitem extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final VoidCallback ontap;
+  const Settitem(
+      {super.key,
+      required this.title,
+      required this.icon,
+      required this.ontap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: ontap,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            SizedBox(
+              width: 20.w,
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 20.sp),
+              ),
+            ),
+            const Spacer(),
+            Icon(icon)
+          ],
+        ),
+      ),
+    );
+  }
+}
